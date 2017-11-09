4152cfd5ec16aa7edbf7d6ab404e495d6078bf08
Ciprian Dorin Craciun <ciprian@volution.ro>
2017-11-09 08:36:25 +0200

    [evaluator]  Add limited expressions and evaluator

---
+++ sources/evaluator.rs
@@ -0,0 +1,140 @@
+
+
+use super::contexts::exports::*;
+use super::errors::exports::*;
+use super::expressions::exports::*;
+use super::primitives::exports::*;
+use super::runtime::exports::*;
+use super::values::exports::*;
+
+
+pub mod exports {
+	
+	pub use super::Evaluator;
+	
+}
+
+
+
+
+pub struct Evaluator {
+}
+
+
+impl Evaluator {
+	
+	
+	pub fn new () -> (Evaluator) {
+		Evaluator {}
+	}
+	
+	
+	
+	
+	pub fn evaluate (&mut self, context : &mut Context, input : &Expression) -> (Outcome<Value>) {
+		
+		match *input {
+			
+			Expression::Void => Ok (NULL.into ()),
+			Expression::Value (ref value) => Ok (value.clone ()),
+			
+			Expression::ContextDefine (ref identifier, ref expression) =>
+				self.evaluate_context_define (context, identifier, expression),
+			Expression::ContextUpdate (ref identifier, ref expression) =>
+				self.evaluate_context_update (context, identifier, expression),
+			Expression::ContextSelect (ref identifier) =>
+				self.evaluate_context_select (context, identifier),
+			
+			Expression::PrimitiveCall0 (primitive) =>
+				self.evaluate_primitive_0 (context, primitive),
+			Expression::PrimitiveCall1 (primitive, ref input) =>
+				self.evaluate_primitive_1 (context, primitive, input),
+			Expression::PrimitiveCall2 (primitive, ref input_1, ref input_2) =>
+				self.evaluate_primitive_2 (context, primitive, input_1, input_2),
+			Expression::PrimitiveCallN (primitive, ref inputs) =>
+				self.evaluate_primitive_n (context, primitive, inputs.as_ref ()),
+			Expression::PrimitiveCall (primitive, ref inputs) =>
+				self.evaluate_primitive (context, primitive, inputs.as_ref ()),
+			
+			_ =>
+				unimplemented! (0xc1942075),
+		}
+		
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_slice (&mut self, context : &mut Context, inputs : &[Expression]) -> (Outcome<Vec<Value>>) {
+		let mut outputs = Vec::with_capacity (inputs.len ());
+		for input in inputs {
+			let output = try! (self.evaluate (context, input));
+			outputs.push (output);
+		}
+		return Ok (outputs);
+	}
+	
+	
+	
+	
+	#[ inline (always) ]
+	pub fn evaluate_context_define (&mut self, context : &mut Context, identifier : &Symbol, expression : &Expression) -> (Outcome<Value>) {
+		let mut binding = try! (context.define (identifier));
+		let value_new = try! (self.evaluate (context, expression));
+		let value_old = binding.set (value_new);
+		return Ok (value_old);
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_context_update (&mut self, context : &mut Context, identifier : &Symbol, expression : &Expression) -> (Outcome<Value>) {
+		let mut binding = try! (context.resolve (identifier));
+		let value_new = try! (self.evaluate (context, expression));
+		let value_old = binding.set (value_new);
+		return Ok (value_old);
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_context_select (&mut self, context : &mut Context, identifier : &Symbol) -> (Outcome<Value>) {
+		let binding = try! (context.resolve (identifier));
+		let value = binding.get ();
+		return Ok (value);
+	}
+	
+	
+	
+	
+	#[ inline (always) ]
+	pub fn evaluate_primitive_0 (&mut self, context : &mut Context, primitive : Primitive0) -> (Outcome<Value>) {
+		let output = primitive_0_evaluate (primitive);
+		return output;
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_primitive_1 (&mut self, context : &mut Context, primitive : Primitive1, input : &Expression) -> (Outcome<Value>) {
+		let input = try! (self.evaluate (context, input));
+		let output = primitive_1_evaluate (primitive, &input);
+		return output;
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_primitive_2 (&mut self, context : &mut Context, primitive : Primitive2, input_1 : &Expression, input_2 : &Expression) -> (Outcome<Value>) {
+		let input_1 = try! (self.evaluate (context, input_1));
+		let input_2 = try! (self.evaluate (context, input_2));
+		let output = primitive_2_evaluate (primitive, &input_1, &input_2);
+		return output;
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_primitive_n (&mut self, context : &mut Context, primitive : PrimitiveN, inputs : &[Expression]) -> (Outcome<Value>) {
+		let inputs = try! (self.evaluate_slice (context, inputs));
+		let output = primitive_n_evaluate (primitive, inputs.as_ref ());
+		return output;
+	}
+	
+	#[ inline (always) ]
+	pub fn evaluate_primitive (&mut self, context : &mut Context, primitive : Primitive, inputs : &[Expression]) -> (Outcome<Value>) {
+		let inputs = try! (self.evaluate_slice (context, inputs));
+		let output = primitive_evaluate (primitive, inputs.as_ref ());
+		return output;
+	}
+	
+}
+
---
+++ sources/expressions.rs
@@ -0,0 +1,89 @@
+
+
+use super::errors::exports::*;
+use super::contexts::exports::*;
+use super::primitives::exports::*;
+use super::runtime::exports::*;
+use super::values::exports::*;
+
+
+
+
+pub mod exports {
+	pub use super::Expression;
+	pub use super::ExpressionBox;
+	pub use super::ExpressionVec;
+}
+
+
+
+
+#[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
+pub enum Expression {
+	
+	Void,
+	Value ( Value ),
+	
+	ContextDefine ( Symbol, ExpressionBox ),
+	ContextUpdate ( Symbol, ExpressionBox ),
+	ContextSelect ( Symbol ),
+	
+	BindingGet ( Binding ),
+	BindingSet ( Binding, ExpressionBox ),
+	
+	PrimitiveCall0 ( Primitive0 ),
+	PrimitiveCall1 ( Primitive1, ExpressionBox ),
+	PrimitiveCall2 ( Primitive2, ExpressionBox, ExpressionBox ),
+	PrimitiveCallN ( PrimitiveN, ExpressionVec ),
+	PrimitiveCall ( Primitive, ExpressionVec ),
+	
+}
+
+
+pub type ExpressionBox = StdBox<Expression>;
+pub type ExpressionVec = StdVec<Expression>;
+
+
+impl StdFrom<Value> for Expression { fn from (value : Value) -> (Self) { Expression::Value (value) } }
+
+impl StdFrom<Boolean> for Expression { fn from (value : Boolean) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<NumberInteger> for Expression { fn from (value : NumberInteger) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<NumberReal> for Expression { fn from (value : NumberReal) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Character> for Expression { fn from (value : Character) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Symbol> for Expression { fn from (value : Symbol) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<String> for Expression { fn from (value : String) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Bytes> for Expression { fn from (value : Bytes) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Pair> for Expression { fn from (value : Pair) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Array> for Expression { fn from (value : Array) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<Error> for Expression { fn from (value : Error) -> (Self) { Expression::Value (value.into ()) } }
+
+impl StdFrom<bool> for Expression { fn from (value : bool) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<i64> for Expression { fn from (value : i64) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<f64> for Expression { fn from (value : f64) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<char> for Expression { fn from (value : char) -> (Self) { Expression::Value (value.into ()) } }
+
+impl StdFrom<StdString> for Expression { fn from (value : StdString) -> (Self) { Expression::Value (value.into ()) } }
+impl StdFrom<&'static str> for Expression { fn from (value : &'static str) -> (Self) { Expression::Value (value.into ()) } }
+
+
+impl StdFrom<Value> for ExpressionBox { fn from (value : Value) -> (Self) { StdBox::new (value.into ()) } }
+
+impl StdFrom<Boolean> for ExpressionBox { fn from (value : Boolean) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<NumberInteger> for ExpressionBox { fn from (value : NumberInteger) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<NumberReal> for ExpressionBox { fn from (value : NumberReal) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Character> for ExpressionBox { fn from (value : Character) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Symbol> for ExpressionBox { fn from (value : Symbol) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<String> for ExpressionBox { fn from (value : String) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Bytes> for ExpressionBox { fn from (value : Bytes) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Pair> for ExpressionBox { fn from (value : Pair) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Array> for ExpressionBox { fn from (value : Array) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<Error> for ExpressionBox { fn from (value : Error) -> (Self) { StdBox::new (value.into ()) } }
+
+impl StdFrom<bool> for ExpressionBox { fn from (value : bool) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<i64> for ExpressionBox { fn from (value : i64) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<f64> for ExpressionBox { fn from (value : f64) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<char> for ExpressionBox { fn from (value : char) -> (Self) { StdBox::new (value.into ()) } }
+
+impl StdFrom<StdString> for ExpressionBox { fn from (value : StdString) -> (Self) { StdBox::new (value.into ()) } }
+impl StdFrom<&'static str> for ExpressionBox { fn from (value : &'static str) -> (Self) { StdBox::new (value.into ()) } }
+
