f144cbd1ed1e38d71b7337aa059de95a9b4b9838
Ciprian Dorin Craciun <ciprian@volution.ro>
2017-11-09 08:29:59 +0200

    [values]  Add value type, implement boolean operations, force inlining, minor others

--- sources/values.rs
+++ sources/values.rs
@@ -1,17 +1,22 @@
 
 
+use super::errors::exports::*;
+use super::runtime::exports::*;
+
 use std::cmp;
 use std::f64;
 use std::fmt;
 use std::hash;
+use std::ops;
 
-use super::errors::exports::*;
-use super::runtime::std::*;
+
 
 
 pub mod exports {
-	pub use super::{Value, Symbol};
-	pub use super::{NULL, TRUE, FALSE};
+	pub use super::{Value, ValueClass};
+	pub use super::{Boolean, NumberInteger, NumberReal, Character, Symbol, String, Bytes, Pair, Array};
+	pub use super::{NULL, VOID, UNDEFINED};
+	pub use super::{TRUE, FALSE};
 	pub use super::{ONE, ZERO};
 	pub use super::{INF_POSITIVE, INF_NEGATIVE, NAN_POSITIVE, NAN_NEGATIVE};
 	pub use super::{number_i64, number_f64, character};
@@ -23,22 +28,105 @@ pub mod exports {
 
 
 #[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
+pub enum ValueClass {
+	
+	Null,
+	Void,
+	Undefined,
+	
+	Boolean,
+	NumberInteger,
+	NumberReal,
+	Character,
+	
+	Symbol,
+	String,
+	Bytes,
+	
+	Pair,
+	Array,
+	
+	Error,
+	
+	Number,
+	List,
+	
+}
+
+
+
+
+#[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
 pub enum Value {
-	Null ( Null ),
+	
+	Null,
+	Void,
+	Undefined,
+	
 	Boolean ( Boolean ),
 	NumberInteger ( NumberInteger ),
 	NumberReal ( NumberReal ),
 	Character ( Character ),
+	
 	Symbol ( Symbol ),
 	String ( String ),
 	Bytes ( Bytes ),
+	
 	Pair ( Pair ),
 	Array ( Array ),
+	
 	Error ( Error ),
+	
 }
 
 
-impl StdFrom<Null> for Value { fn from (value : Null) -> (Self) { Value::Null (value) } }
+impl Value {
+	
+	#[ inline (always) ]
+	pub fn class (&self) -> (ValueClass) {
+		match *self {
+			
+			Value::Null => ValueClass::Null,
+			Value::Void => ValueClass::Void,
+			Value::Undefined => ValueClass::Undefined,
+			
+			Value::Boolean (_) => ValueClass::Boolean,
+			Value::NumberInteger (_) => ValueClass::NumberInteger,
+			Value::NumberReal (_) => ValueClass::NumberReal,
+			Value::Character (_) => ValueClass::Character,
+			
+			Value::Symbol (_) => ValueClass::Symbol,
+			Value::String (_) => ValueClass::String,
+			Value::Bytes (_) => ValueClass::Bytes,
+			
+			Value::Pair (_) => ValueClass::Pair,
+			Value::Array (_) => ValueClass::Array,
+			
+			Value::Error (_) => ValueClass::Error,
+			
+		}
+	}
+	
+	#[ inline (always) ]
+	pub fn is (&self, class : ValueClass) -> (bool) {
+		let class_actual = self.class ();
+		if class_actual == class {
+			return true;
+		} else {
+			match class {
+				List =>
+					return (class_actual == ValueClass::Null) || (class_actual == ValueClass::Pair),
+				Number =>
+					return (class_actual == ValueClass::NumberInteger) || (class_actual == ValueClass::NumberReal),
+				_ =>
+					panic! ("1fa4f499"),
+			}
+		}
+	}
+	
+}
+
+
 impl StdFrom<Boolean> for Value { fn from (value : Boolean) -> (Self) { Value::Boolean (value) } }
 impl StdFrom<NumberInteger> for Value { fn from (value : NumberInteger) -> (Self) { Value::NumberInteger (value) } }
 impl StdFrom<NumberReal> for Value { fn from (value : NumberReal) -> (Self) { Value::NumberReal (value) } }
@@ -50,21 +138,31 @@ impl StdFrom<Pair> for Value { fn from (value : Pair) -> (Self) { Value::Pair (v
 impl StdFrom<Array> for Value { fn from (value : Array) -> (Self) { Value::Array (value) } }
 impl StdFrom<Error> for Value { fn from (value : Error) -> (Self) { Value::Error (value) } }
 
+impl StdFrom<bool> for Value { fn from (value : bool) -> (Self) { Value::Boolean (value.into ()) } }
+impl StdFrom<i64> for Value { fn from (value : i64) -> (Self) { Value::NumberInteger (value.into ()) } }
+impl StdFrom<f64> for Value { fn from (value : f64) -> (Self) { Value::NumberReal (value.into ()) } }
+impl StdFrom<char> for Value { fn from (value : char) -> (Self) { Value::Character (value.into ()) } }
+
+impl StdFrom<StdString> for Value { fn from (value : StdString) -> (Self) { Value::String (value.into ()) } }
+impl StdFrom<&'static str> for Value { fn from (value : &'static str) -> (Self) { Value::String (value.into ()) } }
+
 
 impl fmt::Display for Value {
 	fn fmt (&self, formatter : &mut fmt::Formatter) -> (fmt::Result) {
-		match self {
-			&Value::Null (ref value) => value.fmt (formatter),
-			&Value::Boolean (ref value) => value.fmt (formatter),
-			&Value::NumberInteger (ref value) => value.fmt (formatter),
-			&Value::NumberReal (ref value) => value.fmt (formatter),
-			&Value::Character (ref value) => value.fmt (formatter),
-			&Value::Symbol (ref value) => value.fmt (formatter),
-			&Value::String (ref value) => value.fmt (formatter),
-			&Value::Bytes (ref value) => value.fmt (formatter),
-			&Value::Pair (ref value) => value.fmt (formatter),
-			&Value::Array (ref value) => value.fmt (formatter),
-			&Value::Error (ref value) => value.fmt (formatter),
+		match *self {
+			Value::Null => formatter.write_str ("#null"),
+			Value::Void => formatter.write_str ("#void"),
+			Value::Undefined => formatter.write_str ("#undefined"),
+			Value::Boolean (ref value) => value.fmt (formatter),
+			Value::NumberInteger (ref value) => value.fmt (formatter),
+			Value::NumberReal (ref value) => value.fmt (formatter),
+			Value::Character (ref value) => value.fmt (formatter),
+			Value::Symbol (ref value) => value.fmt (formatter),
+			Value::String (ref value) => value.fmt (formatter),
+			Value::Bytes (ref value) => value.fmt (formatter),
+			Value::Pair (ref value) => value.fmt (formatter),
+			Value::Array (ref value) => value.fmt (formatter),
+			Value::Error (ref value) => value.fmt (formatter),
 		}
 	}
 }
@@ -73,20 +171,59 @@ impl fmt::Display for Value {
 
 
 #[ derive (Clone, Copy, Debug, Eq, PartialEq, Hash) ]
-pub struct Null ();
+pub struct Boolean ( bool );
 
 
-impl fmt::Display for Null {
-	fn fmt (&self, formatter : &mut fmt::Formatter) -> (fmt::Result) {
-		formatter.write_str ("#null")
+impl Boolean {
+	
+	#[ inline (always) ]
+	pub fn not (&self) -> (Boolean) {
+		(!self.0) .into ()
+	}
+	
+	#[ inline (always) ]
+	pub fn and (&self, other : &Boolean) -> (Boolean) {
+		(self.0 && other.0) .into ()
+	}
+	
+	#[ inline (always) ]
+	pub fn or (&self, other : &Boolean) -> (Boolean) {
+		(self.0 || other.0) .into ()
+	}
+	
+	#[ inline (always) ]
+	pub fn xor (&self, other : &Boolean) -> (Boolean) {
+		(self.0 ^ other.0) .into ()
+	}
+	
+	#[ inline (always) ]
+	pub fn nand (&self, other : &Boolean) -> (Boolean) {
+		self.and (other) .not ()
+	}
+	
+	#[ inline (always) ]
+	pub fn nor (&self, other : &Boolean) -> (Boolean) {
+		self.or (other) .not ()
+	}
+	
+	#[ inline (always) ]
+	pub fn nxor (&self, other : &Boolean) -> (Boolean) {
+		self.xor (other) .not ()
 	}
 }
 
 
+impl ops::Not for Boolean {
+	type Output = Boolean;
+	
+	#[ inline (always) ]
+	fn not (self) -> (Boolean) {
+		self.not ()
+	}
+}
 
 
-#[ derive (Clone, Copy, Debug, Eq, PartialEq, Hash) ]
-pub struct Boolean ( bool );
+impl StdFrom<bool> for Boolean { fn from (value : bool) -> (Self) { Boolean (value) } }
 
 
 impl fmt::Display for Boolean {
@@ -105,6 +242,9 @@ impl fmt::Display for Boolean {
 pub struct NumberInteger ( i64 );
 
 
+impl StdFrom<i64> for NumberInteger { fn from (value : i64) -> (Self) { NumberInteger (value) } }
+
+
 impl fmt::Display for NumberInteger {
 	fn fmt (&self, formatter : &mut fmt::Formatter) -> (fmt::Result) {
 		write! (formatter, "{}", self.0)
@@ -116,6 +256,9 @@ impl fmt::Display for NumberInteger {
 
 #[ derive (Clone, Copy, Debug) ]
 pub struct NumberReal ( f64 );
+
+
+impl StdFrom<f64> for NumberReal { fn from (value : f64) -> (Self) { NumberReal (value) } }
 
 
 impl cmp::Eq for NumberReal {}
@@ -145,6 +288,9 @@ impl fmt::Display for NumberReal {
 pub struct Character ( char );
 
 
+impl StdFrom<char> for Character { fn from (value : char) -> (Self) { Character (value) } }
+
+
 impl fmt::Display for Character {
 	fn fmt (&self, formatter : &mut fmt::Formatter) -> (fmt::Result) {
 		use std::fmt::Write;
@@ -164,10 +310,6 @@ impl fmt::Display for Character {
 pub struct Symbol ( StdRc<StdString> );
 
 
-impl StdFrom<StdString> for Symbol { fn from (value : StdString) -> (Self) { Symbol (StdRc::new (value)) } }
-impl StdFrom<&'static str> for Symbol { fn from (value : &str) -> (Self) { Symbol (StdRc::new (StdString::from (value))) } }
-
-
 impl fmt::Display for Symbol {
 	fn fmt (&self, formatter : &mut fmt::Formatter) -> (fmt::Result) {
 		formatter.write_str (self.0.as_str ())
@@ -179,6 +321,10 @@ impl fmt::Display for Symbol {
 
 #[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
 pub struct String ( StdRc<StdString> );
+
+
+impl StdFrom<StdString> for String { fn from (value : StdString) -> (Self) { String (StdRc::new (value)) } }
+impl StdFrom<&'static str> for String { fn from (value : &'static str) -> (Self) { String (StdRc::new (StdString::from (value))) } }
 
 
 impl fmt::Display for String {
@@ -201,7 +347,7 @@ impl fmt::Display for String {
 
 
 #[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
-pub struct Bytes ( StdRc<StdVector<u8>> );
+pub struct Bytes ( StdRc<StdVec<u8>> );
 
 
 impl fmt::Display for Bytes {
@@ -237,7 +383,7 @@ impl fmt::Display for Pair {
 		loop {
 			try! ((head.0).0.fmt (formatter));
 			match (head.0).1 {
-				Value::Null (_) => break,
+				Value::Null => break,
 				Value::Pair (ref tail) => {
 					try! (formatter.write_char (' '));
 					head = tail;
@@ -260,7 +406,7 @@ impl fmt::Display for Pair {
 
 
 #[ derive (Clone, Debug, Eq, PartialEq, Hash) ]
-pub struct Array ( StdRc<StdVector<Value>> );
+pub struct Array ( StdRc<StdVec<Value>> );
 
 
 impl fmt::Display for Array {
@@ -284,7 +430,9 @@ impl fmt::Display for Array {
 
 
 
-pub const NULL : Null = Null ();
+pub const NULL : Value = Value::Null;
+pub const VOID : Value = Value::Void;
+pub const UNDEFINED : Value = Value::Undefined;
 
 pub const TRUE : Boolean = Boolean (true);
 pub const FALSE : Boolean = Boolean (false);
@@ -300,22 +448,27 @@ pub const NAN_NEGATIVE : NumberReal = NumberReal (f64::NAN);
 
 
 
+#[ inline (always) ]
 pub fn number_i64 (value : i64) -> (NumberInteger) {
 	NumberInteger (value)
 }
 
+#[ inline (always) ]
 pub fn number_f64 (value : f64) -> (NumberReal) {
 	NumberReal (value)
 }
 
+#[ inline (always) ]
 pub fn character (value : char) -> (Character) {
 	Character (value)
 }
 
+#[ inline (always) ]
 pub fn symbol (value : &str) -> (Symbol) {
 	Symbol (StdRc::new (StdString::from (value)))
 }
 
+#[ inline (always) ]
 pub fn string (value : &str) -> (String) {
 	String (StdRc::new (StdString::from (value)))
 }
@@ -323,6 +476,7 @@ pub fn string (value : &str) -> (String) {
 
 
 
+#[ inline (always) ]
 pub fn string_from_slice (slice : &[char]) -> (String) {
 	let mut value = StdString::with_capacity (slice.len ());
 	for character in slice {
@@ -331,6 +485,7 @@ pub fn string_from_slice (slice : &[char]) -> (String) {
 	return String (StdRc::new (StdString::from (value)));
 }
 
+#[ inline (always) ]
 pub fn bytes_from_slice (slice : &[u8]) -> (Bytes) {
 	Bytes (StdRc::new (slice.to_vec ()))
 }
@@ -338,10 +493,12 @@ pub fn bytes_from_slice (slice : &[u8]) -> (Bytes) {
 
 
 
+#[ inline (always) ]
 pub fn list_from_slice (slice : &[Value]) -> (Value) {
 	list_from_slice_2 (slice, NULL.into ())
 }
 
+#[ inline (always) ]
 pub fn list_from_slice_2 (slice : &[Value], continuation : Value) -> (Value) {
 	let mut head = continuation.clone ();
 	for value in slice.iter () .rev () {
@@ -353,6 +510,7 @@ pub fn list_from_slice_2 (slice : &[Value], continuation : Value) -> (Value) {
 
 
 
+#[ inline (always) ]
 pub fn array_from_slice (slice : &[Value]) -> (Array) {
 	Array (StdRc::new (slice.to_vec ()))
 }
