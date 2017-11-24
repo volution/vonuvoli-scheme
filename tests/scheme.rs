9f7a092e5e7c502959173f850c4b8047017bc744
Ciprian Dorin Craciun <ciprian@volution.ro>
2017-11-24 22:38:53 +0200

    [tests]  Update tests inline with latest development code

--- tests/scheme.rs
+++ tests/scheme.rs
@@ -9,6 +9,7 @@ extern crate rust_scheme;
 def_tests_from_file! (
 		values => "scheme/values.sst",
 		types => "scheme/types.sst",
+		pairs => "scheme/pairs.sst",
 		lists => "scheme/lists.sst",
 		quotation => "scheme/quotation.sst",
 		booleans => "scheme/booleans.sst",
@@ -16,5 +17,6 @@ def_tests_from_file! (
 		contexts => "scheme/contexts.sst",
 		lambdas => "scheme/lambdas.sst",
 		functions => "scheme/functions.sst",
+		arrays => "scheme/arrays.sst",
 	);
 
