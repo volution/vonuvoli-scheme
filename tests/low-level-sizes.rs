2d91bbdd9621ccff9bcb3cb1a893a8e604b3e5ed
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-01-17 21:15:55 +0200

    [tests]  Update size assertions

--- tests/low-level-sizes.rs
+++ tests/low-level-sizes.rs
@@ -6,60 +6,141 @@ include! ("prelude.in");
 
 
 
+macro_rules! assert_size {
+	( all, $assertion : tt, [ $( $type : ty ),* , ] ) => (
+		eprint! ("\n----------\n");
+		$( assert_size! ($type, $assertion); )*
+		eprint! ("----------\n\n");
+	);
+	( for, [ $( $assertion : tt ),* , ] ) => (
+		eprint! ("\n----------\n");
+		$( assert_size! $assertion ; )*
+		eprint! ("----------\n\n");
+	);
+	( $type : ty, ( like, $other : ty ) ) => (
+		assert_size! ($type, (exact, mem::size_of::<$other> ()));
+	);
+	( $type : ty, ( smaller_than, $other : ty ) ) => (
+		assert_size! ($type, (at_most, mem::size_of::<$other> ()));
+	);
+	( $type : ty, ( exact, $expected : expr ) ) => (
+		assert_size! ($type, report);
+		assert_eq! (mem::size_of::<$type> (), $expected);
+	);
+	( $type : ty, ( at_most, $expected : expr ) ) => (
+		assert_size! ($type, report);
+		assert! (mem::size_of::<$type> () <= $expected);
+	);
+	( $type : ty, report ) => (
+		eprintln! ("## size-of `{}`: {}", stringify! ($type), mem::size_of::<$type> ());
+	);
+}
+
+
 
 def_test! (test__0, {
 	
-	eprintln! ("----------");
-	eprintln! ("## size-of `Value`: {}", mem::size_of::<Value> ());
-	eprintln! ("## size-of `ValueMeta1`: {}", mem::size_of::<ValueMeta1> ());
-	eprintln! ("## size-of `ValueMeta2`: {}", mem::size_of::<ValueMeta2> ());
-	eprintln! ("## size-of `ValueClass`: {}", mem::size_of::<ValueClass> ());
 	
-	eprintln! ("----------");
-	eprintln! ("## size-of `Boolean`: {}", mem::size_of::<Boolean> ());
-	eprintln! ("## size-of `NumberInteger`: {}", mem::size_of::<NumberInteger> ());
-	eprintln! ("## size-of `NumberReal`: {}", mem::size_of::<NumberReal> ());
-	eprintln! ("## size-of `Character`: {}", mem::size_of::<Character> ());
-	eprintln! ("## size-of `Symbol`: {}", mem::size_of::<Symbol> ());
-	eprintln! ("## size-of `StringImmutable`: {}", mem::size_of::<StringImmutable> ());
-	eprintln! ("## size-of `StringMutable`: {}", mem::size_of::<StringMutable> ());
-	eprintln! ("## size-of `BytesImmutable`: {}", mem::size_of::<BytesImmutable> ());
-	eprintln! ("## size-of `BytesMutable`: {}", mem::size_of::<BytesMutable> ());
-	eprintln! ("## size-of `PairImmutable`: {}", mem::size_of::<PairImmutable> ());
-	eprintln! ("## size-of `PairMutable`: {}", mem::size_of::<PairMutable> ());
-	eprintln! ("## size-of `ArrayImmutable`: {}", mem::size_of::<ArrayImmutable> ());
-	eprintln! ("## size-of `ArrayMutable`: {}", mem::size_of::<ArrayMutable> ());
-	eprintln! ("## size-of `Error`: {}", mem::size_of::<Error> ());
-	eprintln! ("## size-of `Lambda`: {}", mem::size_of::<Lambda> ());
-	eprintln! ("## size-of `ProcedurePrimitive`: {}", mem::size_of::<ProcedurePrimitive> ());
-	eprintln! ("## size-of `ProcedureExtended`: {}", mem::size_of::<ProcedureExtended> ());
-	eprintln! ("## size-of `ProcedureNative`: {}", mem::size_of::<ProcedureNative> ());
-	eprintln! ("## size-of `ArithmeticPrimitive1`: {}", mem::size_of::<ArithmeticPrimitive1> ());
-	eprintln! ("## size-of `SyntaxPrimitive`: {}", mem::size_of::<SyntaxPrimitive> ());
-	eprintln! ("## size-of `SyntaxExtended`: {}", mem::size_of::<SyntaxExtended> ());
-	eprintln! ("## size-of `SyntaxNative`: {}", mem::size_of::<SyntaxNative> ());
-	eprintln! ("## size-of `Context`: {}", mem::size_of::<Context> ());
-	eprintln! ("## size-of `Binding`: {}", mem::size_of::<Binding> ());
+	assert_size! (all, report, [
+			Value,
+			ValueMeta1, ValueMeta2,
+			ValueClass, ValueKind,
+		]);
 	
-	eprintln! ("----------");
-	eprintln! ("## size-of `(Value)`: {}", mem::size_of::<(Value)> ());
-	eprintln! ("## size-of `(Value, u8)`: {}", mem::size_of::<(Value, u8)> ());
-	eprintln! ("## size-of `(Value, u8, u8)`: {}", mem::size_of::<(Value, u8, u8)> ());
+	assert_size! (all, (smaller_than, *mut usize), [
+			
+			ValueSingleton,
+			Boolean,
+			NumberInteger, NumberReal,
+			Character,
+			
+			Symbol,
+			StringImmutable,
+			StringMutable,
+			BytesImmutable,
+			BytesMutable,
+			
+			PairImmutable,
+			PairMutable,
+			ArrayImmutable,
+			ArrayMutable,
+			Values,
+			
+			Error,
+			ProcedurePrimitive,
+			ProcedureExtended,
+			ProcedureNative,
+			ProcedureLambda,
+			
+			SyntaxPrimitive,
+			SyntaxExtended,
+			SyntaxNative,
+			SyntaxLambda,
+			
+			Port,
+			Process,
+			
+			Context,
+			Binding,
+			
+		]);
 	
-	eprintln! ("----------");
-	eprintln! ("## size-of `Expression`: {}", mem::size_of::<Expression> ());
 	
-	eprintln! ("----------");
-	eprintln! ("## size-of `()`: {}", mem::size_of::<()> ());
-	eprintln! ("## size-of `Option<()>`: {}", mem::size_of::<Option<()>> ());
-	eprintln! ("## size-of `Option<i8>`: {}", mem::size_of::<Option<i8>> ());
-	eprintln! ("## size-of `Option<i16>`: {}", mem::size_of::<Option<i16>> ());
-	eprintln! ("## size-of `Option<i32>`: {}", mem::size_of::<Option<i32>> ());
-	eprintln! ("## size-of `Option<(i32, i32)>`: {}", mem::size_of::<Option<(i32, i32)>> ());
-	eprintln! ("## size-of `Option<i64>`: {}", mem::size_of::<Option<i64>> ());
-	eprintln! ("----------");
+	assert_size! (all, report, [
+			
+			Expression,
+			
+		]);
 	
-	assert_eq! (mem::size_of::<Value> (), 16);
+	
+	assert_size! (for, [
+			
+			(Value, (like, ([*mut usize; 2]))),
+			(Expression, (smaller_than, ([*mut usize; 8]))),
+			
+			(StdBox<Value>, (like, [*mut usize; 1])),
+			(StdBox<Expression>, (like, [*mut usize; 1])),
+			(Option<StdBox<Value>>, (like, [*mut usize; 1])),
+			(Option<StdBox<Expression>>, (like, [*mut usize; 1])),
+			
+			(StdRc<Value>, (like, [*mut usize; 1])),
+			(StdRc<Expression>, (like, [*mut usize; 1])),
+			(Option<StdRc<Value>>, (like, [*mut usize; 1])),
+			(Option<StdRc<Expression>>, (like, [*mut usize; 1])),
+			
+			(StdRef<Value>, (like, [*mut usize; 2])),
+			(StdRef<Expression>, (like, [*mut usize; 2])),
+			(Option<StdRef<Value>>, (like, [*mut usize; 2])),
+			(Option<StdRef<Expression>>, (like, [*mut usize; 2])),
+			
+			(StdBox<StdAny>, (like, [*mut usize; 2])),
+			(Option<StdBox<StdAny>>, (like, [*mut usize; 2])),
+			(StdRc<StdAny>, (like, [*mut usize; 2])),
+			(Option<StdRc<StdAny>>, (like, [*mut usize; 2])),
+			
+		]);
+	
+	
+	
+	
+	assert_size! (all, report, [
+			
+			StdString,
+			StdVec<Value>,
+			
+		]);
+	
+	
+	assert_size! (all, report, [
+			
+			(), u8, u16, u32, u64, usize, *mut usize,
+			
+			Option<()>, Option<u8>, Option<u16>, Option<u32>, Option<u64>, Option<usize>, Option<*mut usize>,
+			
+			Option<Option<()>>, Option<Option<u8>>, Option<Option<u16>>, Option<Option<u32>>, Option<Option<u64>>, Option<Option<usize>>, Option<Option<*mut usize>>,
+			
+		]);
+	
 	
 });
 
