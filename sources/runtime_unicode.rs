6cacaba88f1ce85ca9d85d6bfb63500c60767794
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-01-25 21:27:55 +0200

    [runtime]  Add support for partially decoding UTF8 buffers

--- sources/runtime_unicode.rs
+++ sources/runtime_unicode.rs
@@ -10,14 +10,23 @@ use super::prelude::*;
 pub mod exports {
 	
 	pub use super::{
+		
 		unicode_utf8_char_width,
 		unicode_utf8_char_decode,
 		unicode_utf8_char_decode_and_width,
+		
+		unicode_utf8_char_decode_slice_consume,
+		unicode_utf8_char_decode_slice_copy_slice,
+		unicode_utf8_char_decode_slice_extend_vector,
+		unicode_utf8_char_decode_slice_extend_string,
+		
 	};
 	
 	pub use super::{
+		
 		unicode_utf8_chars_clone_string,
 		unicode_utf8_string_clone_chars,
+		
 	};
 }
 
@@ -55,6 +64,77 @@ pub fn unicode_utf8_char_decode_and_width (bytes : &[u8]) -> (Outcome<(char, usi
 		let char = try! (unicode_utf8_char_decode (bytes));
 		succeed! ((char, char_width));
 	}
+}
+
+
+
+
+#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+pub fn unicode_utf8_char_decode_slice_consume <Consumer> (bytes : &[u8], limit_count : Option<usize>, limit_char : Option<char>, consumer : Consumer) -> (Outcome<(usize, usize, bool)>)
+		where Consumer : FnMut (char) -> ()
+{
+	let mut consumer = consumer;
+	let mut chars_remaining = limit_count.unwrap_or (usize::max_value ());
+	let mut bytes_remaining = bytes;
+	let mut chars_accumulated = 0;
+	let mut bytes_accumulated = 0;
+	let mut matched = false;
+	loop {
+		if (chars_remaining == 0) || bytes_remaining.is_empty () {
+			break;
+		}
+		let char_byte_0 = bytes_remaining[0];
+		let char_width = unicode_utf8_char_width (char_byte_0);
+		if char_width == 0 {
+			if chars_accumulated > 0 {
+				break;
+			} else {
+				fail! (0x000704f5);
+			}
+		} else if char_width > bytes_remaining.len () {
+			break;
+		} else {
+			let char = if let Some (code) = core_str::next_code_point (&mut bytes_remaining.iter ()) {
+				unsafe { core_char::from_u32_unchecked (code) }
+			} else {
+				if chars_accumulated > 0 {
+					break;
+				} else {
+					fail! (0xc721276f);
+				}
+			};
+			consumer (char);
+			chars_accumulated += 1;
+			bytes_accumulated += char_width;
+			chars_remaining -= 1;
+			bytes_remaining = &bytes_remaining[char_width..];
+			if let Some (limit_char) = limit_char {
+				if limit_char == char {
+					matched = true;
+					break;
+				}
+			}
+		}
+	}
+	succeed! ((chars_accumulated, bytes_accumulated, matched));
+}
+
+
+#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+pub fn unicode_utf8_char_decode_slice_copy_slice (bytes : &[u8], limit_count : Option<usize>, limit_char : Option<char>, target : &mut [char]) -> (Outcome<(usize, usize, bool)>) {
+	let limit_count = Some (usize::min (target.len (), limit_count.unwrap_or (usize::max_value ())));
+	let mut offset = 0;
+	return unicode_utf8_char_decode_slice_consume (bytes, limit_count, limit_char, |char| { target[offset] = char; offset += 1; });
+}
+
+#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+pub fn unicode_utf8_char_decode_slice_extend_vector (bytes : &[u8], limit_count : Option<usize>, limit_char : Option<char>, target : &mut StdVec<char>) -> (Outcome<(usize, usize, bool)>) {
+	return unicode_utf8_char_decode_slice_consume (bytes, limit_count, limit_char, |char| target.push (char));
+}
+
+#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+pub fn unicode_utf8_char_decode_slice_extend_string (bytes : &[u8], limit_count : Option<usize>, limit_char : Option<char>, target : &mut StdString) -> (Outcome<(usize, usize, bool)>) {
+	return unicode_utf8_char_decode_slice_consume (bytes, limit_count, limit_char, |char| target.push (char));
 }
 
 
