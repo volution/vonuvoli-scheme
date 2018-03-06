c147913084b6527de782d185a1bde9be5ab79be3
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-03-06 23:46:22 +0200

    [procedures]  Refactor native procedures to include an additional struct wrapper (which allows implementing traits for)

--- sources/native_procedures.rs
+++ sources/native_procedures.rs
@@ -33,6 +33,24 @@ pub mod exports {
 	
 	pub use super::ProcedureNativeV;
 	
+	pub use super::ProcedureNativeFn0;
+	pub use super::ProcedureNativeFn1;
+	pub use super::ProcedureNativeFn2;
+	pub use super::ProcedureNativeFn3;
+	pub use super::ProcedureNativeFn4;
+	pub use super::ProcedureNativeFn5;
+	pub use super::ProcedureNativeFnN;
+	
+	pub use super::ProcedureNativeFn0E;
+	pub use super::ProcedureNativeFn1E;
+	pub use super::ProcedureNativeFn2E;
+	pub use super::ProcedureNativeFn3E;
+	pub use super::ProcedureNativeFn4E;
+	pub use super::ProcedureNativeFn5E;
+	pub use super::ProcedureNativeFnNE;
+	
+	pub use super::ProcedureNativeFnV;
+	
 	pub use super::super::conversions::{
 			procedure_native_0, procedure_native_0e,
 			procedure_native_1, procedure_native_1e,
@@ -49,32 +67,82 @@ pub mod exports {
 
 
 
-pub type ProcedureNative0 = fn () -> (Outcome<Value>);
-pub type ProcedureNative1 = fn (&Value) -> (Outcome<Value>);
-pub type ProcedureNative2 = fn (&Value, &Value) -> (Outcome<Value>);
-pub type ProcedureNative3 = fn (&Value, &Value, &Value) -> (Outcome<Value>);
-pub type ProcedureNative4 = fn (&Value, &Value, &Value, &Value) -> (Outcome<Value>);
-pub type ProcedureNative5 = fn (&Value, &Value, &Value, &Value, &Value) -> (Outcome<Value>);
-pub type ProcedureNativeN = fn (&[&Value]) -> (Outcome<Value>);
+#[ derive (Clone) ]
+pub struct ProcedureNative0 (pub ProcedureNativeFn0);
 
-pub type ProcedureNative0E = fn (&mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNative1E = fn (&Value, &mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNative2E = fn (&Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNative3E = fn (&Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNative4E = fn (&Value, &Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNative5E = fn (&Value, &Value, &Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
-pub type ProcedureNativeNE = fn (&[&Value], &mut EvaluatorContext) -> (Outcome<Value>);
+#[ derive (Clone) ]
+pub struct ProcedureNative1 (pub ProcedureNativeFn1);
 
-pub type ProcedureNativeV = fn (arguments : usize) -> (Outcome<ProcedureNativeInternals>);
+#[ derive (Clone) ]
+pub struct ProcedureNative2 (pub ProcedureNativeFn2);
 
+#[ derive (Clone) ]
+pub struct ProcedureNative3 (pub ProcedureNativeFn3);
 
+#[ derive (Clone) ]
+pub struct ProcedureNative4 (pub ProcedureNativeFn4);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative5 (pub ProcedureNativeFn5);
+
+#[ derive (Clone) ]
+pub struct ProcedureNativeN (pub ProcedureNativeFnN);
 
 
 #[ derive (Clone) ]
+pub struct ProcedureNative0E (pub ProcedureNativeFn0E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative1E (pub ProcedureNativeFn1E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative2E (pub ProcedureNativeFn2E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative3E (pub ProcedureNativeFn3E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative4E (pub ProcedureNativeFn4E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNative5E (pub ProcedureNativeFn5E);
+
+#[ derive (Clone) ]
+pub struct ProcedureNativeNE (pub ProcedureNativeFnNE);
+
+
+#[ derive (Clone) ]
+pub struct ProcedureNativeV (pub ProcedureNativeFnV);
+
+
+
+
+pub type ProcedureNativeFn0 = fn () -> (Outcome<Value>);
+pub type ProcedureNativeFn1 = fn (&Value) -> (Outcome<Value>);
+pub type ProcedureNativeFn2 = fn (&Value, &Value) -> (Outcome<Value>);
+pub type ProcedureNativeFn3 = fn (&Value, &Value, &Value) -> (Outcome<Value>);
+pub type ProcedureNativeFn4 = fn (&Value, &Value, &Value, &Value) -> (Outcome<Value>);
+pub type ProcedureNativeFn5 = fn (&Value, &Value, &Value, &Value, &Value) -> (Outcome<Value>);
+pub type ProcedureNativeFnN = fn (&[&Value]) -> (Outcome<Value>);
+
+pub type ProcedureNativeFn0E = fn (&mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFn1E = fn (&Value, &mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFn2E = fn (&Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFn3E = fn (&Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFn4E = fn (&Value, &Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFn5E = fn (&Value, &Value, &Value, &Value, &Value, &mut EvaluatorContext) -> (Outcome<Value>);
+pub type ProcedureNativeFnNE = fn (&[&Value], &mut EvaluatorContext) -> (Outcome<Value>);
+
+pub type ProcedureNativeFnV = fn (arguments : usize) -> (Outcome<ProcedureNativeInternals>);
+
+
+
+
+#[ derive (Clone, Debug) ]
 pub struct ProcedureNative ( StdRc<ProcedureNativeInternals> );
 
 
-#[ derive (Clone) ]
+#[ derive (Clone, Debug) ]
 pub enum ProcedureNativeInternals {
 	
 	Native0 (ProcedureNative0),
@@ -117,46 +185,267 @@ impl ProcedureNative {
 	}
 	
 	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		let self_0 = self.internals_ref ();
+		return self_0.symbol ();
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
 	pub fn handle (&self) -> (Handle) {
 		let self_0 = self.internals_ref ();
-		let value = match *self_0 {
-			ProcedureNativeInternals::Native0 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native1 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native2 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native3 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native4 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native5 (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::NativeN (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native0E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native1E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native2E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native3E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native4E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::Native5E (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::NativeNE (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-			ProcedureNativeInternals::NativeV (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-		};
-		return Handle::new (value);
+		return self_0.handle ();
 	}
 	
 	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
 	pub fn is_self (&self, other : &ProcedureNative) -> (bool) {
 		StdRc::ptr_eq (&self.0, &other.0) || Handle::eq (&self.handle (), &other.handle ())
+	}
+}
+
+
+
+
+impl ProcedureNativeInternals {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		match *self {
+			ProcedureNativeInternals::Native0 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native1 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native2 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native3 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native4 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native5 (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::NativeN (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native0E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native1E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native2E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native3E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native4E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::Native5E (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::NativeNE (ref native) =>
+				return native.symbol (),
+			ProcedureNativeInternals::NativeV (ref native) =>
+				return native.symbol (),
+		}
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+
+
+
+impl ProcedureNative0 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative1 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative2 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative3 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative4 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative5 {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNativeN {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+
+impl ProcedureNative0E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative1E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative2E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative3E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative4E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNative5E {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+impl ProcedureNativeNE {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+
+impl ProcedureNativeV {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
 	}
 }
 
--- sources/native_syntaxes.rs
+++ sources/native_syntaxes.rs
@@ -18,21 +18,29 @@ pub mod exports {
 	
 	pub use super::SyntaxNativeG;
 	
+	pub use super::SyntaxNativeFnG;
+	
+	pub use super::super::conversions::syntax_native_g;
+	
 }
 
 
 
 
-pub type SyntaxNativeG = fn (CompilerContext, Value) -> (Outcome<(CompilerContext, Expression)>);
-
-
-
-
 #[ derive (Clone) ]
+pub struct SyntaxNativeG (pub SyntaxNativeFnG);
+
+
+pub type SyntaxNativeFnG = fn (CompilerContext, Value) -> (Outcome<(CompilerContext, Expression)>);
+
+
+
+
+#[ derive (Clone, Debug) ]
 pub struct SyntaxNative ( StdRc<SyntaxNativeInternals> );
 
 
-#[ derive (Clone) ]
+#[ derive (Clone, Debug) ]
 pub enum SyntaxNativeInternals {
 	
 	NativeG (SyntaxNativeG),
@@ -59,18 +67,55 @@ impl SyntaxNative {
 	}
 	
 	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		let self_0 = self.internals_ref ();
+		return self_0.symbol ();
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
 	pub fn handle (&self) -> (Handle) {
 		let self_0 = self.internals_ref ();
-		let value = match *self_0 {
-			SyntaxNativeInternals::NativeG (ref native) =>
-				unsafe { mem::transmute_copy (native) },
-		};
-		return Handle::new (value);
+		return self_0.handle ();
 	}
 	
 	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
 	pub fn is_self (&self, other : &SyntaxNative) -> (bool) {
 		StdRc::ptr_eq (&self.0, &other.0) || Handle::eq (&self.handle (), &other.handle ())
+	}
+}
+
+
+
+
+impl SyntaxNativeInternals {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		match *self {
+			SyntaxNativeInternals::NativeG (ref native) =>
+				return native.symbol (),
+		}
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
+	}
+}
+
+
+
+
+impl SyntaxNativeG {
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn symbol (&self) -> (BacktraceSymbol) {
+		return BacktraceSymbol::new (unsafe { mem::transmute (self.0) });
+	}
+	
+	#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+	pub fn handle (&self) -> (Handle) {
+		return self.symbol () .handle ();
 	}
 }
 
