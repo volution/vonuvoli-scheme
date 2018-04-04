c51dd2ff12402ff678a03987b6d284198115d27a
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-04-04 20:39:46 +0300

    [primitives]  Update extended procedures to support function curry and composition

--- sources/extended_procedures.rs
+++ sources/extended_procedures.rs
@@ -38,6 +38,11 @@ pub struct ProcedureExtended ( StdRc<ProcedureExtendedInternals> );
 pub enum ProcedureExtendedInternals {
 	
 	ComposedPrimitive1 (StdBox<[ProcedurePrimitive1]>),
+	Composed1 (StdBox<[Value]>),
+	ComposedV (StdBox<[Value]>),
+	
+	CurryLeft (Value, StdBox<[Value]>),
+	CurryRight (Value, StdBox<[Value]>),
 	
 	RecordKindIs (RecordKind, Option<bool>),
 	RecordBuild (RecordKind, Option<StdBox<[usize]>>, Option<bool>),
@@ -45,6 +50,10 @@ pub enum ProcedureExtendedInternals {
 	RecordGetX (Option<RecordKind>, Value),
 	RecordSet (Option<RecordKind>, usize),
 	RecordSetX (Option<RecordKind>, Value),
+	
+	Constant (Value, bool),
+	
+	Not (Value),
 	
 }
 
@@ -71,11 +80,24 @@ impl ProcedureExtended {
 
 
 #[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
-pub fn procedure_extended_evaluate_0 (extended : &ProcedureExtended, _evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+pub fn procedure_extended_evaluate_0 (extended : &ProcedureExtended, evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+	const INPUTS_EMPTY : &'static [&'static Value] = &[];
 	match *extended.internals_ref () {
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), INPUTS_EMPTY),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, INPUTS_EMPTY, values.as_ref ()),
 		
 		ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable) =>
 			return record_build_0 (kind, option_box_as_ref (fields), immutable),
+		
+		ProcedureExtendedInternals::Constant (ref value, _) =>
+			succeed! (value.clone ()),
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_0 (callable))) .into_0 (),
 		
 		_ =>
 			fail! (0x9507fccd),
@@ -93,6 +115,18 @@ pub fn procedure_extended_evaluate_1 (extended : &ProcedureExtended, input_1 : &
 		ProcedureExtendedInternals::ComposedPrimitive1 (ref primitives) =>
 			return call_primitives_1 (evaluator, primitives.as_ref (), input_1),
 		
+		ProcedureExtendedInternals::Composed1 (ref callables) =>
+			return call_composed_1 (evaluator, callables.as_ref (), input_1),
+		
+		ProcedureExtendedInternals::ComposedV (ref callables) =>
+			return call_composed_v (evaluator, callables.as_ref (), &[input_1]),
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), &[input_1]),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, &[input_1], values.as_ref ()),
+		
 		ProcedureExtendedInternals::RecordKindIs (ref kind, immutable) =>
 			return record_kind_is (kind, input_1, immutable) .into_0 (),
 		
@@ -105,6 +139,16 @@ pub fn procedure_extended_evaluate_1 (extended : &ProcedureExtended, input_1 : &
 		ProcedureExtendedInternals::RecordGetX (ref kind, ref field) =>
 			return record_get_x (kind.as_ref (), field, input_1),
 		
+		ProcedureExtendedInternals::Constant (ref value, ignore) =>
+			if ignore {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x478a25fd);
+			},
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_1 (callable, input_1))) .into_0 (),
+		
 		_ =>
 			fail! (0x224ed4b5),
 		
@@ -115,8 +159,14 @@ pub fn procedure_extended_evaluate_1 (extended : &ProcedureExtended, input_1 : &
 
 
 #[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
-pub fn procedure_extended_evaluate_2 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, _evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+pub fn procedure_extended_evaluate_2 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
 	match *extended.internals_ref () {
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), &[input_1, input_2]),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, &[input_1, input_2], values.as_ref ()),
 		
 		ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable) =>
 			return record_build_2 (kind, option_box_as_ref (fields), input_1, input_2, immutable),
@@ -126,6 +176,16 @@ pub fn procedure_extended_evaluate_2 (extended : &ProcedureExtended, input_1 : &
 		
 		ProcedureExtendedInternals::RecordSetX (ref kind, ref field) =>
 			return record_set_x (kind.as_ref (), field, input_1, input_2),
+		
+		ProcedureExtendedInternals::Constant (ref value, ignore) =>
+			if ignore {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x478a25fd);
+			},
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_2 (callable, input_1, input_2))) .into_0 (),
 		
 		_ =>
 			fail! (0x786569ea),
@@ -137,11 +197,27 @@ pub fn procedure_extended_evaluate_2 (extended : &ProcedureExtended, input_1 : &
 
 
 #[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
-pub fn procedure_extended_evaluate_3 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, _evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+pub fn procedure_extended_evaluate_3 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
 	match *extended.internals_ref () {
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), &[input_1, input_2, input_3]),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, &[input_1, input_2, input_3], values.as_ref ()),
 		
 		ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable) =>
 			return record_build_3 (kind, option_box_as_ref (fields), input_1, input_2, input_3, immutable),
+		
+		ProcedureExtendedInternals::Constant (ref value, ignore) =>
+			if ignore {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x478a25fd);
+			},
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_3 (callable, input_1, input_2, input_3))) .into_0 (),
 		
 		_ =>
 			fail! (0x3a0174c2),
@@ -153,11 +229,27 @@ pub fn procedure_extended_evaluate_3 (extended : &ProcedureExtended, input_1 : &
 
 
 #[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
-pub fn procedure_extended_evaluate_4 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, input_4 : &Value, _evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+pub fn procedure_extended_evaluate_4 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, input_4 : &Value, evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
 	match *extended.internals_ref () {
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), &[input_1, input_2, input_3, input_4]),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, &[input_1, input_2, input_3, input_4], values.as_ref ()),
 		
 		ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable) =>
 			return record_build_4 (kind, option_box_as_ref (fields), input_1, input_2, input_3, input_4, immutable),
+		
+		ProcedureExtendedInternals::Constant (ref value, ignore) =>
+			if ignore {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x478a25fd);
+			},
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_4 (callable, input_1, input_2, input_3, input_4))) .into_0 (),
 		
 		_ =>
 			fail! (0x25d23c58),
@@ -169,11 +261,27 @@ pub fn procedure_extended_evaluate_4 (extended : &ProcedureExtended, input_1 : &
 
 
 #[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
-pub fn procedure_extended_evaluate_5 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, input_4 : &Value, input_5 : &Value, _evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
+pub fn procedure_extended_evaluate_5 (extended : &ProcedureExtended, input_1 : &Value, input_2 : &Value, input_3 : &Value, input_4 : &Value, input_5 : &Value, evaluator : &mut EvaluatorContext) -> (Outcome<Value>) {
 	match *extended.internals_ref () {
+		
+		ProcedureExtendedInternals::CurryLeft (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, values.as_ref (), &[input_1, input_2, input_3, input_4, input_5]),
+		
+		ProcedureExtendedInternals::CurryRight (ref callable, ref values) =>
+			return call_n_n (evaluator, callable, &[input_1, input_2, input_3, input_4, input_5], values.as_ref ()),
 		
 		ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable) =>
 			return record_build_n (kind, option_box_as_ref (fields), &[input_1, input_2, input_3, input_4, input_5], immutable),
+		
+		ProcedureExtendedInternals::Constant (ref value, ignore) =>
+			if ignore {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x478a25fd);
+			},
+		
+		ProcedureExtendedInternals::Not (ref callable) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_5 (callable, input_1, input_2, input_3, input_4, input_5))) .into_0 (),
 		
 		_ =>
 			fail! (0x80e07b4f),
@@ -192,6 +300,18 @@ pub fn procedure_extended_evaluate_n (extended : &ProcedureExtended, inputs : &[
 		(1, &ProcedureExtendedInternals::ComposedPrimitive1 (ref primitives)) =>
 			return call_primitives_1 (evaluator, primitives.as_ref (), inputs[0]),
 		
+		(1, &ProcedureExtendedInternals::Composed1 (ref callables)) =>
+			return call_composed_1 (evaluator, callables.as_ref (), inputs[0]),
+		
+		(_, &ProcedureExtendedInternals::ComposedV (ref callables)) =>
+			return call_composed_v (evaluator, callables.as_ref (), inputs),
+		
+		(_, &ProcedureExtendedInternals::CurryLeft (ref callable, ref values)) =>
+			return call_n_n (evaluator, callable, values.as_ref (), inputs),
+		
+		(_, &ProcedureExtendedInternals::CurryRight (ref callable, ref values)) =>
+			return call_n_n (evaluator, callable, inputs, values.as_ref ()),
+		
 		(1, &ProcedureExtendedInternals::RecordKindIs (ref kind, immutable)) =>
 			return record_kind_is (kind, inputs[0], immutable) .into_0 (),
 		
@@ -209,6 +329,16 @@ pub fn procedure_extended_evaluate_n (extended : &ProcedureExtended, inputs : &[
 		
 		(_, &ProcedureExtendedInternals::RecordBuild (ref kind, ref fields, immutable)) =>
 			return record_build_n (kind, option_box_as_ref (fields), inputs, immutable),
+		
+		(_, &ProcedureExtendedInternals::Constant (ref value, ignore)) =>
+			if ignore || inputs_count == 0 {
+				succeed! (value.clone ());
+			} else {
+				fail! (0x59dc9d37);
+			},
+		
+		(_, &ProcedureExtendedInternals::Not (ref callable)) =>
+			return is_false (& try! (evaluator.evaluate_procedure_call_n (callable, inputs))) .into_0 (),
 		
 		_ =>
 			fail! (0x7b179cf1),
