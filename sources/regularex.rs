f2e26520b861fb4f778da67b4096baacecaa8b32
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-04-10 20:55:51 +0300

    [regex]  Add support for both string and bytes regular expressions

--- sources/regularex.rs
+++ sources/regularex.rs
@@ -1,3 +1,68 @@
 
-pub mod exports {}
+
+use super::prelude::*;
+
+
+
+
+pub mod exports {
+	
+	pub use super::{
+		StringRegex,
+		BytesRegex,
+	};
+	
+}
+
+
+
+
+macro_rules! define_regex {
+	( $type : ident, $wrapped : ty ) => {
+		
+		
+		#[ derive (Clone, Debug) ]
+		pub struct $type ( StdRc<$wrapped> );
+		
+		
+		impl $type {
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn new (regex : $wrapped) -> ($type) {
+				$type (StdRc::new (regex))
+			}
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn regex_ref (&self) -> (&$wrapped) {
+				&self.0
+			}
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn from_rc (rc : StdRc<$wrapped>) -> ($type) {
+				$type (rc)
+			}
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn clone_rc (rc : &StdRc<$wrapped>) -> ($type) {
+				$type::from_rc (StdRc::clone (rc))
+			}
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn is_self (&self, other : &$type) -> (bool) {
+				StdRc::ptr_eq (&self.0, &other.0)
+			}
+			
+			#[ cfg_attr ( feature = "vonuvoli_inline", inline ) ]
+			pub fn internal_rc_clone (&self) -> (StdRc<$wrapped>) {
+				StdRc::clone (&self.0)
+			}
+		}
+		
+		
+	};
+}
+
+
+define_regex! (StringRegex, regex::Regex);
+define_regex! (BytesRegex, regex::bytes::Regex);
 
