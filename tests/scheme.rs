ac40ed59bf7afc9a9ea38b04b7889c1bf119ccee
Ciprian Dorin Craciun <ciprian@volution.ro>
2018-05-09 10:24:45 +0300

    [tests]  Add tests for hashes builtins

--- tests/scheme.rs
+++ tests/scheme.rs
@@ -200,6 +200,18 @@ def_scheme_tests_from_file! (
 	);
 
 
+#[ cfg ( feature = "vonuvoli_builtins_hashes" ) ]
+def_scheme_tests_from_file! (
+		
+		test__hashes_default => "scheme/hashes-default.sst",
+		test__hashes_siphash => "scheme/hashes-siphash.sst",
+		test__hashes_seahash => "scheme/hashes-seahash.sst",
+		test__hashes_blake2b => "scheme/hashes-blake2b.sst",
+		test__hashes_blake2s => "scheme/hashes-blake2s.sst",
+		
+	);
+
+
 #[ cfg ( feature = "vonuvoli_builtins_serde" ) ]
 def_scheme_tests_from_file! (
 		
