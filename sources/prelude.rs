605317f155e6bd2e4956b9cf77550b378840ae8b
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-06-30 23:02:40 +0300

    [sources]  Update prelude to support 2018 edition.  [#101]

--- sources/prelude.rs
+++ sources/prelude.rs
@@ -1,51 +1,51 @@
 
 
-pub use std::clone::Clone;
-pub use std::default::Default;
-pub use std::marker::Copy;
-pub use std::marker::Sized;
-pub use std::ops::Fn;
-pub use std::ops::FnMut;
-pub use std::ops::FnOnce;
-pub use std::option::Option::None;
-pub use std::option::Option::Some;
-pub use std::option::Option;
-pub use std::result::Result::Err;
-pub use std::result::Result::Ok;
-pub use std::result::Result;
+pub use ::std::clone::Clone;
+pub use ::std::default::Default;
+pub use ::std::marker::Copy;
+pub use ::std::marker::Sized;
+pub use ::std::ops::Fn;
+pub use ::std::ops::FnMut;
+pub use ::std::ops::FnOnce;
+pub use ::std::option::Option::None;
+pub use ::std::option::Option::Some;
+pub use ::std::option::Option;
+pub use ::std::result::Result::Err;
+pub use ::std::result::Result::Ok;
+pub use ::std::result::Result;
 
 
 
 
-pub use std::any::Any as StdAny;
-pub use std::borrow::Borrow as StdBorrow;
-pub use std::boxed::Box as StdBox;
-pub use std::cell::Cell as StdCell;
-pub use std::cell::Ref as StdRef;
-pub use std::cell::RefCell as StdRefCell;
-pub use std::cell::RefMut as StdRefMut;
-pub use std::collections::hash_map::Entry as StdMapEntry;
-pub use std::collections::hash_map::HashMap as StdMap;
-pub use std::collections::hash_set::HashSet as StdSet;
-pub use std::convert::AsRef as StdAsRef;
-pub use std::convert::AsMut as StdAsRefMut;
-pub use std::convert::From as StdFrom;
-pub use std::convert::Into as StdInto;
-pub use std::convert::TryFrom as StdTryFrom;
-pub use std::convert::TryInto as StdTryInto;
-pub use std::ops::Deref as StdDeref;
-pub use std::ops::DerefMut as StdDerefMut;
-pub use std::rc::Rc as StdRc;
-pub use std::sync::Arc as StdArc;
-pub use std::string::String as StdString;
-pub use std::vec::Vec as StdVec;
+pub use ::std::any::Any as StdAny;
+pub use ::std::borrow::Borrow as StdBorrow;
+pub use ::std::boxed::Box as StdBox;
+pub use ::std::cell::Cell as StdCell;
+pub use ::std::cell::Ref as StdRef;
+pub use ::std::cell::RefCell as StdRefCell;
+pub use ::std::cell::RefMut as StdRefMut;
+pub use ::std::collections::hash_map::Entry as StdMapEntry;
+pub use ::std::collections::hash_map::HashMap as StdMap;
+pub use ::std::collections::hash_set::HashSet as StdSet;
+pub use ::std::convert::AsRef as StdAsRef;
+pub use ::std::convert::AsMut as StdAsRefMut;
+pub use ::std::convert::From as StdFrom;
+pub use ::std::convert::Into as StdInto;
+pub use ::std::convert::TryFrom as StdTryFrom;
+pub use ::std::convert::TryInto as StdTryInto;
+pub use ::std::ops::Deref as StdDeref;
+pub use ::std::ops::DerefMut as StdDerefMut;
+pub use ::std::rc::Rc as StdRc;
+pub use ::std::sync::Arc as StdArc;
+pub use ::std::string::String as StdString;
+pub use ::std::vec::Vec as StdVec;
 
 
 
 
-pub use std::eprint;
-pub use std::eprintln;
-pub use std::vec;
+pub use ::std::eprint;
+pub use ::std::eprintln;
+pub use ::std::vec;
 
 
 
@@ -68,84 +68,95 @@ pub use super::externals::regex as regex;
 
 
 
-pub use std::borrow;
-pub use std::cell;
-pub use std::char;
-pub use std::cmp;
-pub use std::collections;
-pub use std::collections::hash_map;
-pub use std::collections::hash_set;
-pub use std::convert;
-pub use std::env;
-pub use std::f32;
-pub use std::f64;
-pub use std::ffi;
-pub use std::fmt;
-pub use std::fs;
-pub use std::hash;
-pub use std::i8;
-pub use std::i16;
-pub use std::i32;
-pub use std::i64;
-pub use std::i128;
-pub use std::io;
-pub use std::isize;
-pub use std::iter;
-pub use std::mem;
-pub use std::num;
-pub use std::ops;
-pub use std::os;
-pub use std::os::unix::io as unix_io;
-pub use std::os::unix::process as unix_process;
-pub use std::panic;
-pub use std::path as fs_path;
-pub use std::process;
-pub use std::ptr;
-pub use std::slice;
-pub use std::str;
-pub use std::string;
-pub use std::thread;
-pub use std::time;
-pub use std::u8;
-pub use std::u16;
-pub use std::u32;
-pub use std::u64;
-pub use std::u128;
-pub use std::usize;
+pub use ::std::borrow;
+pub use ::std::cell;
+pub use ::std::char;
+pub use ::std::cmp;
+pub use ::std::collections;
+pub use ::std::collections::hash_map;
+pub use ::std::collections::hash_set;
+pub use ::std::convert;
+pub use ::std::env;
+pub use ::std::f32;
+pub use ::std::f64;
+pub use ::std::ffi;
+pub use ::std::fmt;
+pub use ::std::fs;
+pub use ::std::hash;
+pub use ::std::i8;
+pub use ::std::i16;
+pub use ::std::i32;
+pub use ::std::i64;
+pub use ::std::i128;
+pub use ::std::io;
+pub use ::std::isize;
+pub use ::std::iter;
+pub use ::std::mem;
+pub use ::std::num;
+pub use ::std::ops;
+pub use ::std::os;
+pub use ::std::os::unix::io as unix_io;
+pub use ::std::os::unix::process as unix_process;
+pub use ::std::panic;
+pub use ::std::path as fs_path;
+pub use ::std::process;
+pub use ::std::ptr;
+pub use ::std::slice;
+pub use ::std::str;
+pub use ::std::string;
+pub use ::std::thread;
+pub use ::std::time;
+pub use ::std::u8;
+pub use ::std::u16;
+pub use ::std::u32;
+pub use ::std::u64;
+pub use ::std::u128;
+pub use ::std::usize;
 
 
 
 
-pub use core::char as core_char;
-pub use core::str as core_str;
+pub use ::core::char as core_char;
+pub use ::core::str as core_str;
 
 
 
 
-pub use std::cmp::Eq as TraitImportEq;
-pub use std::cmp::Ord as TraitImportOrd;
-pub use std::cmp::PartialEq as TraitImportPartialEq;
-pub use std::cmp::PartialOrd as TraitImportPartialOrd;
-pub use std::fmt::Write as TraitImportWriteFmt;
-pub use std::hash::Hasher as TraitImportHasher;
-pub use std::io::BufRead as TraitImportBufRead;
-pub use std::io::Read as TraitImportRead;
-pub use std::io::Write as TraitImportWrite;
-pub use std::iter::ExactSizeIterator as TraitImportExactSizeIterator;
-pub use std::iter::Extend as TraitImportExtend;
-pub use std::iter::FromIterator as TraitImportFromIterator;
-pub use std::iter::IntoIterator as TraitImportIntoIterator;
-pub use std::iter::Iterator as TraitImportIterator;
-pub use std::ops::Deref as TraitImportDeref;
-pub use std::str::FromStr as TraitImportFromStr;
-pub use std::string::ToString as TraitImportToString;
-pub use std::os::unix::ffi::OsStrExt as TraitImportOsStrExt;
-pub use std::os::unix::ffi::OsStringExt as TraitImportOsStringExt;
-pub use std::os::unix::fs::FileTypeExt as TraitImportFileTypeExt;
-pub use std::os::unix::fs::MetadataExt as TraitImportMetadataExt;
-pub use std::os::unix::fs::PermissionsExt as TraitImportPermissionsExt;
+pub use ::std::cmp::Eq as TraitImportEq;
+pub use ::std::cmp::Ord as TraitImportOrd;
+pub use ::std::cmp::PartialEq as TraitImportPartialEq;
+pub use ::std::cmp::PartialOrd as TraitImportPartialOrd;
+pub use ::std::fmt::Write as TraitImportWriteFmt;
+pub use ::std::hash::Hasher as TraitImportHasher;
+pub use ::std::io::BufRead as TraitImportBufRead;
+pub use ::std::io::Read as TraitImportRead;
+pub use ::std::io::Write as TraitImportWrite;
+pub use ::std::iter::ExactSizeIterator as TraitImportExactSizeIterator;
+pub use ::std::iter::Extend as TraitImportExtend;
+pub use ::std::iter::FromIterator as TraitImportFromIterator;
+pub use ::std::iter::IntoIterator as TraitImportIntoIterator;
+pub use ::std::iter::Iterator as TraitImportIterator;
+pub use ::std::ops::Deref as TraitImportDeref;
+pub use ::std::str::FromStr as TraitImportFromStr;
+pub use ::std::string::ToString as TraitImportToString;
+pub use ::std::os::unix::ffi::OsStrExt as TraitImportOsStrExt;
+pub use ::std::os::unix::ffi::OsStringExt as TraitImportOsStringExt;
+pub use ::std::os::unix::fs::FileTypeExt as TraitImportFileTypeExt;
+pub use ::std::os::unix::fs::MetadataExt as TraitImportMetadataExt;
+pub use ::std::os::unix::fs::PermissionsExt as TraitImportPermissionsExt;
 
 
 #[ cfg ( feature = "vonuvoli_builtins_ports" ) ]
 pub use super::ports::PortQueries as TraitImportPortQueries;
+
+
+
+
+pub use ::std::r#try;
+
+pub use ::std::format;
+pub use ::std::write;
+pub use ::std::writeln;
+
+pub use ::lazy_static::lazy_static;
 
